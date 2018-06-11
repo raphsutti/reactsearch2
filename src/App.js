2a46abaf86dda5f1e91a3393040ee5a9331f6830
raphsutti <neoster9@gmail.com>
2018-06-11 17:52:31 +1000

    Const and Let vs var

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import './App.css';
 
 class App extends Component {
   render() {
-    var helloWorld = 'Welcome to the Road to learn React';
-    var answer = 42;
+    const helloWorld = 'Welcome to the Road to learn React practice by Raph Sutti';
+    const answer = 42;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
           <h2>{helloWorld}</h2>
           <p>The answer to everything is {answer}</p>
       </div>
