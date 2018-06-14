18886498c543c68ec6d46dfd51e9d3d06e021dc0
raphsutti <neoster9@gmail.com>
2018-06-14 17:45:39 +1000

    Split up components

--- src/App.js
+++ src/App.js
@@ -78,20 +78,49 @@ class App extends Component {
         <h2>{welcome}</h2>
         <p>The answer to everything is {answer}</p>
         
-        <form>
-          <input type="text" value={searchTerm} onChange={this.onSearchChange} />
-        </form>
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+
+        <Table 
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input type="text" value={value} 
+        onChange={onChange} />
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
         <h3>The list:</h3>
-        {list.filter(isSearched(searchTerm)).map(item => 
-            <div key={item.objectID}>
-              <span><a href={item.url}>{item.title}</a> </span>
-              <span>Author: {item.author} </span>
-              <span>Comments: {item.num_comments} </span>
-              <span>Points: {item.points} </span>
-              <span>
-                <button onClick={() => this.onDismiss(item.objectID)} type="button">Dismiss</button>
-              </span>
-            </div>
+        {list.filter(isSearched(pattern)).map(item => 
+          <div key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a> </span>
+            <span>Author: {item.author} </span>
+            <span>Comments: {item.num_comments} </span>
+            <span>Points: {item.points} </span>
+            <span>
+              <button onClick={() => onDismiss(item.objectID)} type="button">Dismiss</button>
+            </span>
+          </div>
         )}
       </div>
     );
